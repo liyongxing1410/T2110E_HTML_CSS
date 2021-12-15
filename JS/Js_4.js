f67acf01e5d5be1bb3b80824d71e1d24aef5582a
Le Vinh Hung <lv.hung69@gmail.com>
2021-12-15 15:43:42 +0700

    Changing img

--- JS/Js_4.js
+++ JS/Js_4.js
@@ -3,34 +3,21 @@ const container = document.querySelector(".container");
 const url =
   "https://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
 
+const imgUrl = "https://openweathermap.org/img/wn/";
+
 const render = async (url) => {
   const res = await fetch(url);
   const datas = await res.json();
   console.log(datas.list);
   datas.list.forEach((data) => {
     const item = document.createElement("div");
-    let classIcon;
-    switch (data.weather[0].main) {
-      case "Clear":
-        classIcon = "fas fa-sun";
-        break;
-
-      case "Rain":
-        classIcon = "fas fa-cloud-showers-heavy";
-        break;
-
-      case "Clouds":
-        classIcon = "fas fa-cloud";
-        break;
-    }
     item.className = "items";
     item.innerHTML = `
     <h4 class="time">${data.dt_txt}</h4>
     <h3 class="temp">${data.main.temp} <sup>o</sup>C</h3>
     <p class="weather">${data.weather[0].main}</p>
-    <i class="${classIcon}"></i>
+    <img src="${imgUrl}${data.weather[0].icon}@2x.png">
     `;
-
     container.appendChild(item);
   });
 };
